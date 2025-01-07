882c3ac1edef6902b405562744ffacf52e20b5d7
ixy <91363864+ixy-194@users.noreply.github.com>
2025-01-07 01:19:48 +0000

    Fix fizzbuzz

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -7,14 +7,13 @@ import (
 // FizzBuzz は整数 n を受け取り、以下のルールに従って文字列を返します。
 // - n が 3 で割り切れる場合は "Fizz" を返します。
 // - n が 5 で割り切れる場合は "Buzz" を返します。
-// - n が 3 と 5 の両方で割り切れる場合は "FizzBuzz" を返します。
 // - 上記のいずれにも該当しない場合は、n をそのまま文字列に変換して返します。
 func FizzBuzz(n int) string {
 	switch {
-	// - n が 3 と 5で割り切れる場合は "FizzBuzz" を返します。
+	// - n が 3 と 5 で割り切れる場合は "FizzBuzz" を返します。
 	case n%3 == 0 && n%5 == 0:
 		return "FizzBuzz"
-	// - n が 3 で割り切れる場合は "Fizz" を返します。
+		// - n が 3 で割り切れる場合は "Fizz" を返します。
 	case n%3 == 0:
 		return "Fizz"
 	// - n が 5 で割り切れる場合は "Buzz" を返します。
